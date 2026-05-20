63842a27aa2f53f9db2fb9cef2ecc28912009a50
Jeroen Kuilman <jeroenkuilman.webdesign@gmail.com>
2026-05-20 21:41:06 +0200

    First working version of the guessing game is done

--- game.js
+++ game.js
@@ -1,32 +1,80 @@
 "use strict";
 
-//variables
+//elements variables
 const title = document.querySelector(".title");
+const tracker = document.querySelector(".progress-tracker");
+const attemptsCounter = document.querySelector(".attempt-counter");
+
+//button variable
 const btnsPlay = document.querySelectorAll(".btn-play");
+const btnReset = document.querySelector(".btn-reset");
 
 //functions
-const randomNumberCall = () => Math.ceil(Math.random() * 10);
+const randomNumberCall = () => Math.ceil(Math.random() * 12);
 const randomNumber = randomNumberCall();
 
-let clickCounter = 0;
+// the max amount of attempts
+let clickCounter;
+let playing;
+
+///////////////////////////////////////
+//functions
+//////////////////////////////////////
+const init = function () {
+  clickCounter = 5;
+  playing = true;
+
+  tracker.textContent = "click to start...";
+  attemptsCounter.textContent = `Attempts left: ${clickCounter}`;
+
+  btnsPlay.forEach((btn, i) => {
+    btn.classList.remove("right-button");
+    btn.classList.remove("wrong-button");
+    btn.classList.remove("btn-visible");
+    btn.style.backgroundColor = "";
+    btn.style.transform = "";
+  });
+
+  setTimeout(() => {
+    btnsPlay.forEach((btn, i) => {
+      btn.textContent = i + 1;
+      btn.classList.add("btn-visible");
+    });
+  }, 1000);
+};
+
+/////////////////////////////////////
 //eventhandlers
+/////////////////////////////////////
+//logic for clicking the play buttons
+init();
+
+btnReset.addEventListener("click", function (e) {
+  init();
+});
+
 btnsPlay.forEach((btn) =>
   btn.addEventListener("click", function (e) {
     e.preventDefault();
     console.log(clickCounter);
     console.log(randomNumber);
+    if (playing) {
+      if (+btn.textContent === randomNumber) {
+        tracker.textContent = "You win!";
+        btn.classList.add("right-button");
+        playing = false;
+      } else {
+        tracker.textContent = "Guess again!";
+        clickCounter--;
+        attemptsCounter.textContent = `Attempts left: ${clickCounter}`;
+        btn.classList.add("wrong-button");
+        btn.textContent = "";
+      }
 
-    if (+btn.textContent === randomNumber) {
-      title.textContent = "You win!";
-    } else {
-      title.textContent = "Guess again!";
-      clickCounter++;
-    }
-
-    if (clickCounter === 3) {
-      return (title.textContent = "You lose");
+      if (clickCounter === 0) {
+        tracker.textContent = "You lose";
+        playing = false;
+      }
     }
   }),
 );
-
-console.log();
